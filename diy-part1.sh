a22f063c89db3838cfbfc1b5c57f7f5cd5c82f57
shailer66 <412979135@qq.com>
2025-08-03 09:42:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,15 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# Add a feed source
+# lean_source
+echo 'src-git packages_lean https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git luci_lean https://github.com/coolsnowwolf/luci.git;openwrt-24.10' >>feeds.conf.default
+echo 'src-git routing_lean https://github.com/coolsnowwolf/routing' >>feeds.conf.default
+echo 'src-git telephony_lean https://github.com/coolsnowwolf/telephony.git' >>feeds.conf.default
+# kenzok8_source
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
